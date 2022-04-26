0875441d166d6f13e88003c1ae3ee7249533429d
pfigz <pfigdev@gmail.com>
2022-04-26 22:57:41 +0200

    Create folder for Chat vue files, setup basic file structure and templates. Copy dashboard template for main chat view.

--- resources/js/Pages/Chat/container.vue
+++ resources/js/Pages/Chat/container.vue
@@ -1,21 +1,25 @@
-<script setup>
+<script>
 import AppLayout from '@/Layouts/AppLayout.vue';
-import Welcome from '@/Jetstream/Welcome.vue';
 
+export default {
+    components: {
+        AppLayout    
+    }
+}
 </script>
 
 <template>
     <AppLayout title="Dashboard">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
+                Chat
             </h2>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
+                   Container
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\ChatController;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,3 +34,17 @@ Route::middleware([
         return Inertia::render('Dashboard');
     })->name('dashboard');
 });
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/chat', function () {
+        return Inertia::render('Chat/container');
+    })->name('chat');
+});
+
+Route::middleware('auth:sanctum')->get('/chat/rooms', [ChatController::class, 'rooms']);
+Route::middleware('auth:sanctum')->get('/chat/room/{roomID}/messages', [ChatController::class, 'messages']);
+Route::middleware('auth:sanctum')->post('/chat/room/{roomID}/message', [ChatController::class, 'newMessage']);
