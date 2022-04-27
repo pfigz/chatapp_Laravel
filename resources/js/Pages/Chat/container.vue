284fe5f951ceaeccc034e9e1138d2f8a60278cd1
pfigz <pfigdev@gmail.com>
2022-04-27 13:38:56 +0200

    Add methods to parent element to get rooms and messages. Begin prep for input message.

--- resources/js/Pages/Chat/container.vue
+++ resources/js/Pages/Chat/container.vue
@@ -1,9 +1,49 @@
 <script>
 import AppLayout from '@/Layouts/AppLayout.vue';
+import MessageContainer from './messageContainer.vue';
+import InputMessage from './inputMessage.vue';
+import axios from 'axios';
 
 export default {
     components: {
-        AppLayout    
+        AppLayout,
+        MessageContainer,
+        InputMessage    
+    },
+    data: function() {
+        return {
+            chatRooms: [],
+            currentRoom: [],
+            messages: []
+        }
+    },
+    methods: {
+        getRooms() {
+            axios.get('/chat/rooms')
+            .then(response => {
+                this.chatRooms = response.data;
+                this.setRoom(response.data[0]);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+        },
+        setRoom(room) {
+            this.currentRoom = room;
+            this.getMessages();
+        },
+        getMessages() {
+            axios.get('/chat/room/' + this.currentRoom.id + '/messages')
+            .then(response => {
+                this.messages = response.data;
+            })
+            .catch(error => {
+                console.log(error);
+            })
+        }
+    },
+    created() {
+        this.getRooms();
     }
 }
 </script>
@@ -19,7 +59,8 @@ export default {
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                   Container
+                   <message-container />
+                   <input-message :room="currentRoom" />
                 </div>
             </div>
         </div>
